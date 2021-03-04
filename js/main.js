a561f513d1633872ecc411d029feb0ba3f4c40b9
TahiruMustapha <tahirumustapha132@gmail.com>
2021-03-04 13:04:07 +0200

    init project

---
+++ js/main.js
@@ -0,0 +1,15 @@
+console.log("Hellooo......")
+let b = 79
+console.log(b)
+let name = 40000
+console.log(name)
+let school = "KsTU"
+console.log(school)
+let work = ("i love working tahiru")
+console.log(work)
+let age = "16"
+console.log("i am"  + age  +  "years old.")
+let age = "19"
+let nane = ("Tahiru")
+let school = ("KsTU")
+console.log("my name is" + name + "i attend school at" + school + "and i am" + age + "years old")
